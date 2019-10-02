45e32643a3ee1e33f38848979d69711e1632778f
ADA2029617 <ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edu>
2019-10-02 11:44:49 -0700

    Finished adding outlets and actions to view controller.swift

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //------------Outlets-------------//
+    @IBOutlet weak var AppSignOutlet: UILabel!
+    @IBOutlet weak var GameStatusLabel: UILabel!
+    @IBOutlet weak var PlayerRockSignLabel: UIButton!
+    @IBOutlet weak var PlayerPaperSignLabel: UIButton!
+    @IBOutlet weak var PlayerScissorsSignLabel: UIButton!
+    @IBOutlet weak var PlayAgainLabel: UIButton!
+    
+    
+    //------------Actions-------------//
+    @IBAction func PlayerRockButton(_ sender: Any) {
+    }
+    @IBAction func PlayerPaperButton(_ sender: Any) {
+    }
+    @IBAction func PlayerScissorsButton(_ sender: Any) {
+    }
+    @IBAction func PlayAgainButton(_ sender: Any) {
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
